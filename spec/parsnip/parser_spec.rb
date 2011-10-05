907dd9701a5d4924eb62db439a3ccc22a4671114
Nathan Sobo <nathansobo@gmail.com>
2011-10-04 18:11:27 -0700

    Spec storage and expiration of multiple nodes

--- spec/parsnip/parser_spec.rb
+++ spec/parsnip/parser_spec.rb
@@ -18,7 +18,7 @@ describe "Parser" do
       parser.parse('abz').should be_false
     end
 
-    it "expires memo entries correctly" do
+    it "stores and expires memo entries correctly" do
       parser.parse('abc').should be_true
       memo_entry = parser.retrieve(:root, 0)
       memo_entry.value.should == true
@@ -42,9 +42,43 @@ describe "Parser" do
       end
     }}
 
-    it "parses matching input successfully" do
+    it "accepts matching input" do
       parser.parse('alpha bravo').should be_true
+    end
+
+    it "rejects non-matching input" do
       parser.parse('alpha charlie').should be_false
+    end
+
+    it "stores and expires memo entries correctly" do
+      parser.parse('alpha bravo')
+                   #0123456789ab
+
+      root = parser.retrieve(:root, 0)
+      root.value.should == true
+      root.range.should == (0..11)
+
+      a = parser.retrieve(:a, 0)
+      a.value.should == true
+      a.range.should == (0..5)
+
+      b = parser.retrieve(:b, 6)
+      b.value.should == true
+      b.range.should == (6..11)
+
+      parser.update(9..9, 'nd') # alpha brando
+
+      # keep a, it wasn't disturbed
+      a = parser.retrieve(:a, 0)
+      a.value.should == true
+      a.range.should == (0..5)
+
+      # discard root and b
+      parser.retrieve(:root, 0).should be_nil
+      parser.retrieve(:b, 6).should be_nil
+
+      parser.parse.should be_false
+      parser.retrieve(:b, 6).value.should be_false
     end
   end
 end
